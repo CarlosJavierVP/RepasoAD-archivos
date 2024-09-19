d2f9ed8d793b74dc7374cef53d71421593aa78ed
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-09-19 11:59:49 +0200

    Commit leer archivos2 y copiar archivo

--- src/main/java/com/rep/Main.java
+++ src/main/java/com/rep/Main.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
 
+        /*
         //File es un puntero que apunta a archivos o carpetas
         File f = new File(".");
         for(String s : f.list()){
@@ -23,18 +24,62 @@ public class Main {
             throw new RuntimeException(e);
         }
 
-        // try con recursos, cuando lo termine de usar cierra el flujo automaticamente
-        try (BufferedReader bfr = new BufferedReader(new FileReader("pom.xml"))){
-            String s;
-            while ((s = bfr.readLine()) != null){
-                System.out.println(s);
-            }
+         */
+
+
+        String archivo = "pom.xml";
+        leerArchivo(archivo);
+        leerArchivo2(archivo);
+        copiarArchivoTexto("pom.xml", "pom2.xml");
+
+    }
+
+    private static void copiarArchivoTexto(String archivo, String archivo2){
+        try(BufferedReader bfr = new BufferedReader(new FileReader(archivo));
+            BufferedWriter bfw = new BufferedWriter(new FileWriter(archivo2))){
+                String linea;
+                Integer contador = 0;
+                while ((linea = bfr.readLine()) != null ){
+                    contador++;
+                    bfw.write(contador +": "+linea);
+                    bfw.newLine();
+                }
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    private static void leerArchivo(String archivo) {
+        // try con recursos, cuando lo termine de usar cierra el flujo automaticamente
+        try (BufferedReader bfr = new BufferedReader(new FileReader(archivo))){
+            String s;
+            Integer contador =0;
+            while ((s = bfr.readLine()) != null){
+                System.out.println(s);
+                contador++;
+            }
+            System.out.println("Este archivo tiene "+contador+" líneas");
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //lee el archivo entero y luego se procesa
+    private static void leerArchivo2(String archivo){
+        try (BufferedReader bfr = new BufferedReader(new FileReader(archivo))){
+         Integer lineas = bfr.lines().toList().size();
+            System.out.println(lineas);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
