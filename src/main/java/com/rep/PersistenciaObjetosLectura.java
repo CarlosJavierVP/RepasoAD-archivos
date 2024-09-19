de11e255b69c0bacc8df760b38e5b03d98e0ff43
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-09-19 13:31:10 +0200

    lectura objetos

--- src/main/java/com/rep/PersistenciaObjetosLectura.java
+++ src/main/java/com/rep/PersistenciaObjetosLectura.java
@@ -1,4 +1,19 @@
 package com.rep;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class PersistenciaObjetosLectura {
+    public static void main(String[] args) {
+
+        try (var ois = new ObjectInputStream(new FileInputStream("Videojuego.ser"))){
+            Videojuego v = (Videojuego) ois.readObject();
+            System.out.println(v);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
